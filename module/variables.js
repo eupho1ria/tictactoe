1bc7e7632606afdf5be48c93f6d868f93e2722d5
eupho1ria <96536276+eupho1ria@users.noreply.github.com>
2022-02-20 18:48:36 +0600

    Update variables.js

--- module/variables.js
+++ module/variables.js
@@ -1,8 +1,8 @@
-/** all the varialbes */
+
 
 export let GAME = {
-    X_CLASS : 'x', // value for female // x as oo's
-    Y_CLASS : 'y', // value for male  // y as xx's
+    X_CLASS : 'x', 
+    Y_CLASS : 'y', 
     turn : undefined,
     winner : null,
     selectedProfile :  document.querySelectorAll(".img .id"),
